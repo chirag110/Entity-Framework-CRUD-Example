c1a173c9d6cdf85670b11351cd8b0800e3841db3
chirag110 <chiragguglani@yahoo.in>
2021-08-21 16:48:33 +0530

    EntityFrameworkCRUD

---
+++ EntityFrameworkCRUDExample/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EntityFrameworkCRUDExample.Models;
+namespace EntityFrameworkCRUDExample.Controllers
+{
+ public class HomeController: Controller
+ {
+ CompanyDbContext db = new CompanyDbContext();
+
+    public ActionResult Index()
+    {
+        List<Employee> emps = db.Employees.ToList();
+        return View(emps);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Employee emp)
+    {
+        db.Employees.Add(emp);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+        Employee emp = db.Employees.Find(id);
+        return View(emp);
+    }
+
+    public ActionResult Edit(int id)
+    { 
+        Employee emp = db.Employees.Find(id);
+        return View(emp);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Employee e)
+    {
+        Employee emp = db.Employees.Where(temp => temp.EmpID == e.EmpID).FirstOrDefault();
+        emp.EmpName = e.EmpName;
+        emp.Salary = e.Salary;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        Employee emp = db.Employees.Where(temp => temp.EmpID == id).FirstOrDefault();
+        return View(emp);
+    }
+
+    [HttpPost]
+    public ActionResult Delete(int id, Employee e)
+    {
+        Employee emp = db.Employees.Where(temp => temp.EmpID == id).FirstOrDefault();
+        db.Employees.Remove(emp);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
+}
---
+++ EntityFrameworkCRUDExample/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+namespace EntityFrameworkCRUDExample.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public decimal Salary { get; set; }
+    }
+    public class CompanyDbContext: DbContext
+ {
+ public CompanyDbContext() : base(@"data source=localhost\sqlexpress; integrated security=yes; initial catalog=company")
+    {
+    }
+    public DbSet<Employee> Employees { get; set; }
+}
+}
